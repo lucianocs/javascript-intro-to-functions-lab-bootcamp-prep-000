aec2cb6b3db40e52a6c61cfdc4b56e24b3fcba4a
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-11 22:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string === string.toLowerCase ) {
+  } else if (string === string.toLowerCase() ) {
     return "YES INDEED!"
   }else if (string === "I love you, Grandma.") {
     return "I love you, too."
